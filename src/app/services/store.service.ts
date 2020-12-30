3353abbc04d4b69c44404274bd4a8be0ab76de0b
kshab <kvshabalev@gmail.com>
2020-12-30 12:45:01 +0200

    feature/add-firebase: add-firebase

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private fireStore: AngularFirestore) { }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB2waqmhWturuZ_y8h45asPHh89HBS7f1s',
+    authDomain: 'ng-todo-c3e25.firebaseapp.com',
+    projectId: 'ng-todo-c3e25',
+    storageBucket: 'ng-todo-c3e25.appspot.com',
+    messagingSenderId: '68909516675',
+    appId: '1:68909516675:web:1656384fa3e7ce44b3d8b6'
+  }
 };
 
 /*
